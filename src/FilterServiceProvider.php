4daf882e8242f488162b5ed39728dfd5e8f37b33
kadirmelihcan <kadirmelih.can@clevel.team>
2020-04-01 02:28:38 +0300

    Added Nova 3 support, laravel 6 & 7 support, advanced multilang added

--- src/FilterServiceProvider.php
+++ src/FilterServiceProvider.php
@@ -1,33 +1,43 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Kdrmlhcn\NovaMultiselectFilter;
 
-use Laravel\Nova\Nova;
-use Laravel\Nova\Events\ServingNova;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Nova\Nova;
 
 class FilterServiceProvider extends ServiceProvider
 {
+    protected const SCRIPT_FILE = __DIR__ . '/../dist/js/filter.js';
+    protected static $availableTranslations = [
+        'tr',
+        'de'
+    ];
+
     /**
      * Bootstrap any application services.
      *
      * @return void
      */
-    public function boot()
+    public function boot(): void
     {
-        Nova::serving(function (ServingNova $event) {
-            Nova::script('nova-multiselect-filter', __DIR__.'/../dist/js/filter.js');
-            Nova::style('nova-multiselect-filter', __DIR__.'/../dist/css/filter.css');
+        Nova::serving(static function () {
+            Nova::script('nova-multiselect-label-filter', self::SCRIPT_FILE);
+
+            $translations = FilterServiceProvider::getTranslationsFromAppLocale();
+
+            if(!is_null($translations))
+                Nova::translations($translations);
         });
     }
 
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
+    public static function getTranslationsFromAppLocale()
     {
-        //
+        $locale = App::getLocale();
+
+        if(in_array($locale, static::$availableTranslations))
+            return __DIR__ . "/../resources/lang/$locale/filter.json";
+
+        return null;
     }
 }
--- src/NovaMultiselectFilter.php
+++ src/NovaMultiselectFilter.php
@@ -1,7 +1,9 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Kdrmlhcn\NovaMultiselectFilter;
 
+use InvalidArgumentException;
+use Kdrmlhcn\NovaMultiselectFilter\Enums\Config;
 use Laravel\Nova\Filters\Filter;
 
 abstract class NovaMultiselectFilter extends Filter
@@ -11,5 +13,25 @@ abstract class NovaMultiselectFilter extends Filter
      *
      * @var string
      */
-    public $component = 'nova-multiselect-filter';
+    public $component = 'nova-multiselect-label-filter';
+
+    public function __construct(array $configuration = [])
+    {
+        $this->configure($configuration);
+    }
+
+    protected function configure(array $configuration): void
+    {
+        if (empty($configuration)) {
+            return;
+        }
+
+        foreach ($configuration as $property => $value) {
+            if (!in_array($property, Config::getProperties(), true)) {
+                throw new InvalidArgumentException('Invalid property: ' . $property);
+            }
+
+            $this->withMeta([$property => $value]);
+        }
+    }
 }
